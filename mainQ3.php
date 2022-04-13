6f2607249696d20082ace9f2b918e2b4c75f5762
SatoshiTakiguchi <satoshi.takiguchi@tokyo.supersoftware.co.jp>
2022-04-13 17:04:07 +0900

    Q3

--- mainQ3.php
+++ mainQ3.php
@@ -1,5 +1,22 @@
 <?php
 require 'Classes/Cars.php';
 
+function RandomCreateHonda(){
+    $res_list = [];
+    $num = mt_rand(1,20);
+    $res_list[] = new Honda();
+}
+
+$honda = new Honda(400,8);
+$nissan = new Nissan(300);
+$ferrari = new Ferrari(1000);
+
+$ferrari->showCarData();
+
+$ferrari->heightChange();
+$ferrari->showCarData();
+
+$ferrari->heightChange();
+$ferrari->showCarData();
 
 ?>
